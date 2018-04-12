ab3f7af98b4fb56f085457aa4e14948b30c99ecd
afeiship <1290657123@qq.com>
2018-04-12 08:17:13 +0800

    issue: use element ui

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './app'
-import router from './router'
-import {ReduxBoot} from 'next-vue-redux';
-
+import Vue from 'vue';
+import App from './app';
+import router from './router';
+import { ReduxBoot } from 'next-vue-redux';;
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 
-ReduxBoot.run(App,{
+Vue.use(ElementUI);
+
+ReduxBoot.run(App, {
   router,
-  vue:Vue,
-  el:'#app'
+  vue: Vue,
+  el: '#app'
 });
--- src/views/page1/index.vue
+++ src/views/page1/index.vue
@@ -2,13 +2,25 @@
   <div class="page1-view">
     <h1>Page1 content</h1>
     <div class="bd">{{msg}}</div>
+
+
+    <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
 <script>
 import {ReduxAppBase} from 'next-vue-redux';
+import { Button, Select } from 'element-ui';
 export default {
   name: 'page1',
+  components:{
+    Button, Select
+  },
   data () {
     return {
       msg: 'Welcome to Your page1.js App'
