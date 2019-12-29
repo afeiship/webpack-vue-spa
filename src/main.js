1dc8ab6d5fe645110736c9661ff610dff31955dc
afeiship <1290657123@qq.com>
2019-12-29 13:19:03 +0800

    chore: use vue-cli

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './app';
-import router from './router';
-import { ReduxBoot } from 'next-vue-redux';;
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from "vue";
+import Vuex from "vuex";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
-
-ReduxBoot.run(App, {
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
   router,
-  vue: Vue,
-  el: '#app'
+  components: { App },
+  template: "<App/>"
 });
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Page1 from '@/views/page1/index';
-import Page2 from '@/views/page2/index';
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'page1',
-      component: Page1
-    },
-    {
-      path: '/page1',
-      name: 'page1',
-      component: Page1
-    },
-    {
-      path: '/page2',
-      name: 'page2',
-      component: Page2
+      path: "/",
+      name: "HelloWorld",
+      component: HelloWorld
     }
   ]
-})
+});
